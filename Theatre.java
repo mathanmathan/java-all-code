fa569907e2727bb64bb22a69c665bb5c219d76ff
Mathan Kumar <maddymadhan085@gamil.com>
2022-10-09 23:46:56 +0530

    task theatre return  method

--- Theatre.java
+++ Theatre.java
@@ -2,16 +2,16 @@ class Theatre{
 
 public static void main (String[] args){
  
-Theatre raja = new Theatre();
-int balance=raja.bookTicket(200);
-System.out.println("After book ticket "+balance); 
+Theatre raja = new Theatre(); //raja object creation
+int balance=raja.bookTicket(200); //int balance return value and bookTicket(200)args passing
+System.out.println("After book ticket "+balance); // + symbol  concadination operators
 }
 
-int bookTicket(int price){
+int bookTicket(int price){      //int price agrs taken
 int ticket=price;
-int ticket_price=120;
-int balance = ticket-ticket_price;
-return balance;
+int ticket_price=120;           // 120 assign local variable int ticket_price
+int balance = ticket-ticket_price; // local variable int balance
+return balance;             //return type balance value 
 
 }
 
